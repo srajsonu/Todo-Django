ddef5bd59942319f7a1ddb328089b99c660db53c
SONU RAJ <srajsonu02@gmail.com>
2020-09-19 00:16:19 +0530

    models added

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256)
+
+class Task(models.Model): #table
+    content = models.TextField() #column
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField()
+    completed_at = models.DateTimeField()
+    tags = models.ManyToManyField(Tag)
+
+    class TaskStatus(models.TextChoices):
+        COMPLETED = "CO", "Completed"
+        PENDING = "PE", "Pending"
+        DROPPED = "DR", "Dropped"
+
+    status = models.CharField(
+        choices=TaskStatus.choices,
+        default=TaskStatus.PENDING,
+        max_length=2
+    )
+
+
+
+
+
+
--- todo_project/settings.py
+++ todo_project/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tasks.apps.TasksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
